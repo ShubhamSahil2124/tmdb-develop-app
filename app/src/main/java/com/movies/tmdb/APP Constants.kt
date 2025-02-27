aae551ab9a5205f5d3b3bf717c1ebeb4edb5c835
ShubhShwDev <87120835+ShubhShwDev@users.noreply.github.com>
2025-02-28 01:12:23 +0530

    Removed unused code

--- app/src/main/java/com/movies/tmdb/APP Constants.kt
+++ app/src/main/java/com/movies/tmdb/APP Constants.kt
@@ -4,11 +4,6 @@ package com.movies.tmdb
 class AppConstants {
 
     companion object {
-        const val ON_THE_AIR = "on_the_air"
-        const val POPULAR = "popular"
-        const val TOP_RATED = "top_rated"
-        const val AIRING_TODAY = "airing_today"
-        const val NOW_PLAYING = "now_playing"
         const val API_KEY = "9e46ba7771d636cc3527c1f8729e3232"
         const val SPLASH_TIMED_OUT : Long = 4000
     }
